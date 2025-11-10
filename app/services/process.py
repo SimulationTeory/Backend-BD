036f08fea9d346557cb780fbe021e03b91d16a9a
Blandin <almendarezbictor@gmail.com>
2025-11-10 17:52:04 -0600

    Cambios en las funciones

--- app/services/process.py
+++ app/services/process.py
@@ -28,7 +28,7 @@ class Process:
        self.graphCalc.calc_last(graph,early)
        holgura = self.graphCalc.calc_holgura(graph)
        crticalPath = self.graphCalc.calCriticalPath(graph)
-       #self.printGraph(graph,crticalPath["criticalPath"])
+       
        dataNodes = self.graphCalc.dataGraph(graph)
        self.respose.resposeCaseA(crticalPath,dataNodes)
        
@@ -69,27 +69,6 @@ class Process:
 
         
 
-    def printGraph(self,graph,criticalPath):
-         g = graph
-         pos = nx.circular_layout(g)
-         weights = nx.get_edge_attributes(g,"weight")
-          
-         edgesColor = []
-         edges = graph.edges()
-
-         for nodeI,nodeF in edges:
-             edge = f"{nodeI}-{nodeF}"
-             if edge in criticalPath:
-              edgesColor.append("red")
-             else :
-              edgesColor.append("blue")
-             
-
-
-
-         nx.draw(g,pos,with_labels=True,node_size=800,node_color="lightblue",edge_color=edgesColor)
-         nx.draw_networkx_edge_labels(g,pos,edge_labels= weights, font_color="red")
-         pl.savefig("grafo.png")
          
         
 
